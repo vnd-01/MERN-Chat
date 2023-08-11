af6033570f22412faede377c781c9fb2e17b0156
Vikas <vikas01nd@gmail.com>
2023-08-11 13:43:32 +0530

    updated URLs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Pages from "./Pages";
 
 function App() {
-  axios.defaults.baseURL = "https://mern-chat-api-vnd.vercel.app";
+  axios.defaults.baseURL = "//mern-chat-api-vnd.vercel.app";
   axios.defaults.withCredentials = true;
   
   return (
