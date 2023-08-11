e432d0fca500f4ac3344a0240ff2d650ebcbab15
Vikas <vikas01nd@gmail.com>
2023-08-11 13:56:09 +0530

    updated origin

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "https://mern-chat-client-vnd.vercel.app",
+    origin: "//mern-chat-client-vnd.vercel.app",
   })
 );
 
